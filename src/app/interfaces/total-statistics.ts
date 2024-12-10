a0fefafc71da7c90ab7ea5373157e67e5c618d6c
Jesús Daza <jesus.daza@solucionex.com>
2024-12-10 22:43:05 +0100

    feat: New interface total data response

---
+++ src/app/interfaces/total-statistics.ts
@@ -0,0 +1,12 @@
+export interface TotalStatistics {
+  accumulatedTotalExpense: number;
+  accumulatedTotalIncome: number;
+  accumulatedTotalPercentageProfit: number;
+  accumulatedTotalProfit: number;
+  accumulatedStartToEndDateExpense: number;
+  accumulatedStartToEndDateIncome: number;
+  accumulatedStartToEndDatePercentageProfit: number;
+  accumulatedStartToEndDateProfit: number;
+  endDate: string;
+  shopsFinancialStatistics: any[];
+}
