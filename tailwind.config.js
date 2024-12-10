0a3b5f3536c4dc4e865f4c0304e56aa89e1331e3
Jesús Daza <jesus.daza@solucionex.com>
2024-12-10 22:42:01 +0100

    feat: Add tailwind library and tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}" // Asegúrate de incluir todos los archivos HTML y TypeScript en tu proyecto Angular
+  ],
+  theme: {
+    extend: {}, // Aquí puedes personalizar el tema si lo necesitas
+  },
+  plugins: [],
+}
