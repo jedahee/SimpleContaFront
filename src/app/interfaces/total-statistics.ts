aa3bf25c34f81de85935bace334758004e3246a5
Jesús Daza <jesus.daza@solucionex.com>
2024-12-12 22:10:33 +0100

    feat: update name to TotalStatistics interface

--- src/app/interfaces/total-statistics.ts
+++ src/app/interfaces/total-statistics.ts
@@ -1,4 +1,4 @@
-export interface TotalStatistics {
+export interface ITotalStatistics {
   accumulatedTotalExpense: number;
   accumulatedTotalIncome: number;
   accumulatedTotalPercentageProfit: number;
