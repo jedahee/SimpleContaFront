11df3f3d69d580e629914023d27d42670f05e189
Jesús Daza <jesus.daza@solucionex.com>
2024-12-12 22:09:12 +0100

    feat: Add flowbite for fast css design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}" // Asegúrate de incluir todos los archivos HTML y TypeScript en tu proyecto Angular
+    "./src/**/*.{html,ts}", // Asegúrate de incluir todos los archivos HTML y TypeScript en tu proyecto Angular
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {}, // Aquí puedes personalizar el tema si lo necesitas
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
