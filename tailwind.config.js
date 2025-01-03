c36c43926340d37929774e15d3249e055d300842
Jesús Daza <jesus.daza@solucionex.com>
2025-01-03 01:28:12 +0100

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}", // Asegúrate de incluir todos los archivos HTML y TypeScript en tu proyecto Angular
-    "./node_modules/flowbite/**/*.js"
+    './src/**/*.{html,ts,css,scss}', // Asegúrate de incluir todas las rutas
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
-    extend: {}, // Aquí puedes personalizar el tema si lo necesitas
+    extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
   ],
-}
+};
