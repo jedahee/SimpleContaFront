6348504e6450a55be0a3ac5d9cd025677c7d3bd6
Jesús Daza <jesus.daza@solucionex.com>
2025-01-03 01:32:01 +0100

    feat: make header data dinamically

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RequestService } from '../../services/request.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   standalone: true, // Este es clave
 
 })
 export class HeaderComponent {
+  private request = inject(RequestService);
+  private authService = inject(AuthService);
+  actual_site_text = "Cuentas globales"
+  router = inject(Router);
 
+  shops$!: Observable<any[]>;
+  shops: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // Realiza la primera petición para obtener los shops
+    this.shops$ = this.request.getShops();
+
+  }
+
+  changeActualSite(text:string) {
+    this.actual_site_text = text;
+  }
+
+  logout() {
+    this.authService.logout()
+    this.router.navigate(['/login'])
+  }
 }
