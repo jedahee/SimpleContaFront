ad657f2186bcf4e92f5a1183dcf8b44ec11450eb
Jesús Daza <jesus.daza@solucionex.com>
2025-01-17 00:26:51 +0100

    feat: add loader to app component v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
   title = 'Contabilidad';
+
+  constructor() {
+  }
 }
