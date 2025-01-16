7ecb0a0422f26df8cf90d5508d80aa8d53c21873
Jesús Daza <jesus.daza@solucionex.com>
2025-01-17 00:47:43 +0100

    fix: fix daily log table

--- src/app/components/layouts/daily-log-table/daily-log-table.component.ts
+++ src/app/components/layouts/daily-log-table/daily-log-table.component.ts
@@ -1,95 +1,162 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { RequestService } from '../../../services/request.service';
 import { Observable } from 'rxjs';
 import { HttpParams } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { IAddAcounting } from '../../../interfaces/add-acounting';
-import { NgOptimizedImage } from '@angular/common';
+import { ErrorService } from '../../../services/error.service';
 import { forkJoin } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
+import { LoaderService } from '../../../services/loader.service';
+import { NotificationComponent } from '../../notification/notification.component';
+import { ExcelService } from '../../../services/excel.service';
+import { Accordion } from 'flowbite';
 
 @Component({
   selector: 'app-daily-log-table',
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NotificationComponent],
   templateUrl: './daily-log-table.component.html',
   styleUrl: './daily-log-table.component.css',
   standalone: true,
 })
-
-export class DailyLogTableComponent {
+export class DailyLogTableComponent implements OnChanges {
   private request = inject(RequestService);
+  private loader = inject(LoaderService);
+  private errorService = inject(ErrorService);
+  private excelService = inject(ExcelService);
+
   isAccordionOpen: boolean[] = []; // Estado inicial del acordeón
   shops$!: Observable<any[]>;
   logs: any;
+  logs_excel: any;
   error: string = ''; // Para manejar posibles errores
 
-  ngOnInit(): void {
-    // Realiza la primera petición para obtener los shops
-    this.shops$ = this.request.getShops();
+  @Input() shopSelected!: any;
+  @Input() shopsObservable!: any;
+  @Input() from: any;
+  @Input() to: any;
 
-    // Primera petición para obtener los shops
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['shopSelected'] || changes['from'] || changes['to']) {
+      this.isAccordionOpen.forEach((accordion, index) => {
+        this.isAccordionOpen[index] = false;
+      })
+      if (this.shopSelected) {
+        this.fetchDataForSingleShop(); // Fetch para un único shop
+      } else {
+        this.fetchDataForAllShops(); // Fetch para todos los shops
+      }
+    }
+  }
+
+  downloadExcel(): void {
+    this.excelService.exportToExcel(this.logs_excel, 'Cuentas tiendas');
+  }
+
+
+  ngOnInit(): void {
+    if (!this.shopSelected) {
+      this.fetchDataForAllShops(); // Inicialización sin selección de shop
+    }
+    this.errorService.clearErrorMessage();
+
+  }
+
+  // Lógica para obtener datos de todas las tiendas
+  private fetchDataForAllShops(): void {
+    this.shops$ = this.shopsObservable;
+    this.loader.show(); // Mostrar loader
+
     this.shops$
       .pipe(
-        // Extraer los IDs de los shops
-        map((shops: any[]) => shops.map(shop => shop.id)),
-
-        // Realizar una petición por cada ID
+        map((shops: any[]) => shops.map(shop => shop.id)), // Extraer los IDs de las tiendas
         mergeMap((ids: string[]) => {
-          // Crear un array de observables (una petición por ID)
           const requests = ids.map(id => {
             const params = new HttpParams()
-              .set('from', '1900-01-01T00:00:00Z')
-              .set('to', '2025-12-31T23:59:59Z')
+              .set('from', this.from)
+              .set('to', this.to)
               .set('id', id);
 
             return this.request.getAcounting(params); // Llama a tu servicio
           });
-
-          // Combina todas las respuestas en un único array
           return forkJoin(requests);
         })
       )
       .subscribe({
         next: (responses) => {
-          // Asumiendo que `responses` contiene tus subarrays originales
-          this.logs = responses
-            .map((subArray, index) =>
-              subArray.map(item => {
-                const mergedTransactions = [
-                  ...item.listOfIncomes.map((income: any) => ({
-                    ...income,
-                    type: 'income' // Identificar como ingreso
-                  })),
-                  ...item.listOfExpenses.map((expense: any) => ({
-                    ...expense,
-                    type: 'expense' // Identificar como gasto
-                  }))
-                ];
-
-                return {
-                  ...item, // Mantener todas las propiedades originales del item
-                  shop: index === 0 ? 'Villegas' : 'Santa Aurelia', // Asignar la propiedad "source"
-                  transactions: mergedTransactions // Agregar el array combinado
-                };
-              })
-            )
-            .flat() // Combina los subarrays en uno solo
-            .sort((a: any, b: any) => new Date(b.date).getTime() - new Date(a.date).getTime()); // Ordenar por fecha
-
-          console.log(this.logs); // Aquí tienes las respuestas ordenadas con transacciones fusionadas
-
+          console.log(responses)
+          this.logs = this.processResponses(responses);
+          console.log(this.logs)
+          this.logs_excel = responses;
         },
         error: (error) => {
-          console.error('Error en la cadena de peticiones:', error);
+          const message = this.errorService.getErrorMessage(error.status, error.message);
+          this.errorService.setErrorMessage(message);
+          setTimeout(() => {
+            this.loader.hide();
+          }, 0);
         },
         complete: () => {
-          console.log('Todas las peticiones han finalizado');
-        }
+          setTimeout(() => {
+            this.loader.hide();
+          }, 0);
+        },
       });
   }
 
+  // Lógica para obtener datos de una sola tienda
+  private fetchDataForSingleShop(): void {
+    this.loader.show(); // Mostrar loader
 
-  toggleAccordion(id:number) {
+    const params = new HttpParams()
+      .set('from', this.from)
+      .set('to', this.to)
+      .set('id', this.shopSelected.id);
+
+    this.request.getAcounting(params).subscribe({
+      next: (response) => {
+        this.logs = this.processResponses([response]);
+        console.log(`Datos para la tienda ${this.shopSelected.name}:`, this.logs);
+      },
+      error: (error) => {
+        const message = this.errorService.getErrorMessage(error.status, error.message);
+        this.errorService.setErrorMessage(message);
+        this.loader.hide();
+      },
+      complete: () => {
+        this.loader.hide();
+      },
+    });
+  }
+
+  // Procesar respuestas para combinar datos
+  private processResponses(responses: any[][]): any[] {
+
+    return responses
+      .map((subArray, index) =>
+        subArray.map(item => {
+          const mergedTransactions = [
+            ...item.listOfIncomes.map((income: any) => ({
+              ...income,
+              type: 'income' // Identificar como ingreso
+            })),
+            ...item.listOfExpenses.map((expense: any) => ({
+              ...expense,
+              type: 'expense' // Identificar como gasto
+            })),
+          ];
+
+          return {
+            ...item, // Mantener todas las propiedades originales del item
+            shop: responses.length > 1 ? index === 0 ? 'Villegas' : 'Santa Aurelia' : this.shopSelected.name, // Asignar la propiedad "source"
+            transactions: mergedTransactions // Agregar el array combinado
+          };
+        })
+      )
+      .flat() // Combina los subarrays en uno solo
+      .sort((a: any, b: any) => new Date(b.date).getTime() - new Date(a.date).getTime()); // Ordenar por fecha
+  }
+
+  toggleAccordion(id: number): void {
     this.isAccordionOpen[id] = !this.isAccordionOpen[id]; // Alternar el estado abierto/cerrado
   }
 }
