1e1e341dd4a15a8d1b0d0f4ff322454367ecb6c6
Jesús Daza <jesus.daza@solucionex.com>
2025-01-17 00:50:12 +0100

    feat: add routes for components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { GetStatisticsComponent } from './components/get-statistics/get-statistics.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './guards/auth.guard';
+import { DailyAccountingComponent } from './components/daily-accounting/daily-accounting.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: GetStatisticsComponent, canActivate: [authGuard] },
+  { path: 'dailyAccounting', component: DailyAccountingComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: 'login' },
 ];
