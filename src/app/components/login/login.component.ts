a9aab08ee78c771dbf58ce4b282656beaedc4d81
Jesús Daza <jesus.daza@solucionex.com>
2025-01-17 00:46:14 +0100

    fix: fix login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NotificationComponent } from '../notification/notification.component';
+import { ErrorService } from '../../services/error.service';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, NotificationComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   standalone: true
@@ -16,19 +19,38 @@ export class LoginComponent {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private errorService: ErrorService
   ) {
+
+    if (typeof window === 'undefined')
+      this.router.navigate(['https://google.com'])
+
     this.loginForm = this.fb.group({
       password: ['', Validators.required],
     });
+
+    this.errorService.clearErrorMessage();
   }
 
+
   onSubmit(): void {
+
     if (this.loginForm.valid) {
       const { password } = this.loginForm.value;
-      this.authService.login(password).subscribe(() => {
-        this.router.navigate(['/home']); // Redirige a la página principal
+
+      this.authService.login(password).subscribe({
+        next: () => {
+          this.router.navigate(['/home']); // Redirige a la página principal
+        },
+        error: (error) => {
+          const message = this.errorService.getErrorMessage(error.status, error.message);
+          this.errorService.setErrorMessage(message);
+        },
+        complete: () => {
+        }
       });
     }
   }
+
 }
