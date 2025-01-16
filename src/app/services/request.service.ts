1d7c47f11fb1be62d0e0158c54c7e15f10083724
Jesús Daza <jesus.daza@solucionex.com>
2025-01-17 00:46:33 +0100

    fix: fix request service

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environments';
-import { of } from 'rxjs';
 import { ITotalStatistics } from '../interfaces/total-statistics';
 import { IAddAcounting } from '../interfaces/add-acounting';
 
@@ -11,7 +10,7 @@ import { IAddAcounting } from '../interfaces/add-acounting';
 })
 
 export class RequestService {
-  private apiUrl = environment.apiUrl;
+  private readonly apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
